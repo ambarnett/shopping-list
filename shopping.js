5e8d6ae1bee74efd1f36744af99b442a7b1b6c42
ambarnett <andy.barnett09@gmail.com>
2021-05-28 16:44:04 -0500

    first commit for shopping list project

---
+++ shopping.js
@@ -0,0 +1,39 @@
+const foods = [
+    {
+        id: 1,
+        name: "Milk",
+        amount: 1,
+        price: 2.50
+    },
+    {
+        id: 2,
+        name: "Bread",
+        amount: 1,
+        price: 1.50
+    },
+    {
+        id: 3,
+        name: "Eggs",
+        amount: 2,
+        price: 1.89
+    },
+    {
+        id: 4,
+        name: "Greek Yogurt",
+        amount: 3,
+        price: 3
+    },
+    {
+        id: 5,
+        name: "Tuna",
+        amount: 8,
+        price: 0.78
+    },
+    {
+        id: 6,
+        name: "Coffee",
+        amount: 2,
+        price: 3.89
+    }
+]
+console.log(foods)
